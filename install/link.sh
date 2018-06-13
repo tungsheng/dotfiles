69123ebff229602578d64eb699e9fdaf42228a5b
Tony Lee <tungsheng@gmail.com>
2018-06-13 23:41:47 +0800

    remove unused code

--- install/link.sh
+++ install/link.sh
@@ -32,25 +32,3 @@ for config in $DOTFILES/config/*; do
     fi
 done
 
-# create vim symlinks
-# As I have moved off of vim as my full time editor in favor of neovim,
-# I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
-# as it is not really being actively maintained. However, I would still
-# like to configure vim, so lets symlink ~/.vimrc and ~/.vim over to their
-# neovim equivalent.
-
-echo -e "\n\nCreating vim symlinks"
-echo "=============================="
-VIMFILES=( "$HOME/.vim:$DOTFILES/vim/.vim"
-        "$HOME/.vimrc:$DOTFILES/vim/.vimrc" )
-
-for file in "${VIMFILES[@]}" ; do
-    KEY=${file%%:*}
-    VALUE=${file#*:}
-    if [ -e ${KEY} ]; then
-        echo "${KEY} already exists... skipping"
-    else
-        echo "Creating symlink for $KEY"
-        ln -s ${VALUE} ${KEY}
-    fi
-done
