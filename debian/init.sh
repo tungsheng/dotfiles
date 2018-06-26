109de78e03dd00571a8e8f5837a18c00a12be4b6
Tony Lee <tungsheng@gmail.com>
2018-06-27 02:08:32 +0800

    add debian init script

---
+++ debian/init.sh
@@ -0,0 +1,48 @@
+log_file=~/install_progress_log.txt
+
+
+
+echo -ne "Initiating...\n"
+sudo apt-get update
+sudo apt-get -y update
+
+echo -ne "Installing neovim...\n"
+sudo apt-get install neovim
+sudo apt-get install python-neovim
+sudo apt-get install python3-neovim
+
+echo -ne "Installing zsh...\n"
+sudo apt-get -y install zsh
+sudo apt-get install zsh-syntax-highlighting
+if type -p zsh > /dev/null; then
+    echo "zsh Installed" >> $log_file
+else
+    echo "zsh FAILED TO INSTALL!!!" >> $log_file
+fi
+
+echo -ne "Installing curl...\n"
+sudo apt-get -y install curl
+if type -p curl > /dev/null; then
+    echo "curl Installed" >> $log_file
+else
+    echo "crul FAILED TO INSTALL!!!" >> $log_file
+fi
+
+echo -ne "Installing tmux...\n"
+sudo apt-get -y install tmux
+if type -p tmux > /dev/null; then
+    echo "tmux Installed" >> $log_file
+else
+    echo "tmux FAILED TO INSTALL!!!" >> $log_file
+fi
+
+
+echo -ne "Installing silversearcher...\n"
+sudo apt-get install silversearcher-ag
+if type -p ag > /dev/null; then
+    echo "Silver searcher Installed" >> $log_file
+else
+    echo "Silver searcher FAILED TO INSTALL!!!" >> $log_file
+fi
+
+
