fc4d5ffb04b322f117084e2aa505c1366598284f
Tony Lee <tungsheng@gmail.com>
2018-06-27 02:44:07 +0800

    update debian script

--- debian/init.sh
+++ debian/init.sh
@@ -5,11 +5,12 @@ log_file=~/install_progress_log.txt
 echo -ne "Initiating...\n"
 sudo apt-get update
 sudo apt-get -y update
+sudo apt-get -y install openssh-server sudo
 
 echo -ne "Installing neovim...\n"
 sudo apt-get install neovim
-sudo apt-get install python-neovim
-sudo apt-get install python3-neovim
+# sudo apt-get install python-neovim
+# sudo apt-get install python3-neovim
 
 echo -ne "Installing zsh...\n"
 sudo apt-get -y install zsh
@@ -20,14 +21,6 @@ else
     echo "zsh FAILED TO INSTALL!!!" >> $log_file
 fi
 
-echo -ne "Installing curl...\n"
-sudo apt-get -y install curl
-if type -p curl > /dev/null; then
-    echo "curl Installed" >> $log_file
-else
-    echo "crul FAILED TO INSTALL!!!" >> $log_file
-fi
-
 echo -ne "Installing tmux...\n"
 sudo apt-get -y install tmux
 if type -p tmux > /dev/null; then
@@ -36,8 +29,16 @@ else
     echo "tmux FAILED TO INSTALL!!!" >> $log_file
 fi
 
+echo -ne "Installing utils...\n"
+sudo apt-get -y install git tig 
+sudo apt-get -y install whois
+sudo apt-get -y install curl
+if type -p curl > /dev/null; then
+    echo "curl Installed" >> $log_file
+else
+    echo "crul FAILED TO INSTALL!!!" >> $log_file
+fi
 
-echo -ne "Installing silversearcher...\n"
 sudo apt-get install silversearcher-ag
 if type -p ag > /dev/null; then
     echo "Silver searcher Installed" >> $log_file
@@ -45,4 +46,10 @@ else
     echo "Silver searcher FAILED TO INSTALL!!!" >> $log_file
 fi
 
+sudo apt-get -y install python-pip
+if type -p pip > /dev/null; then
+    echo "pip Installed" >> $log_file
+else
+    echo "pip FAILED TO INSTALL!!!" >> $log_file
+fi
 
