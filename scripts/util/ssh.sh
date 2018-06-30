c78af9d0a032150d7ff87ff259ff4d9f238cd628
Tony Lee <tungsheng@gmail.com>
2018-07-01 00:19:51 +0800

    scp use ssh config

--- scripts/util/ssh.sh
+++ scripts/util/ssh.sh
@@ -2,5 +2,7 @@
 cat ~/.ssh/id_rsa.pub | ssh demo@168.58.111.0 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >>  ~/.ssh/authorized_keys"
 
 # copy ssh file
-scp ~/.ssh/id_rsa root@206.189.148.67:~/.ssh
-scp ~/.ssh/id_rsa.pub root@206.189.148.67:~/.ssh
+function sendkey() {
+    scp ~/.ssh/id_rsa $1:~/.ssh
+    scp ~/.ssh/id_rsa.pub $1:~/.ssh
+}
