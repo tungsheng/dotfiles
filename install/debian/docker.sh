9f0b543902fde543b28730a9f12890fc7d7a30df
Tony Lee <tungsheng@gmail.com>
2018-07-01 01:11:36 +0800

    update docker script

--- install/debian/docker.sh
+++ install/debian/docker.sh
@@ -1,7 +1,11 @@
+# switch user
+su docker
+
 # remove older docker
 sudo apt-get -y remove docker docker-engine docker.io
 
 # setup docker repository
+# wget --quiet --output-document - https://download.docker.com/linux/debian/gpg  | sudo apt-key add -
 wget https://download.docker.com/linux/debian/gpg 
 sudo apt-key add gpg
 echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
