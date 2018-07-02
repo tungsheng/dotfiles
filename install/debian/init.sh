0d58f50db6b279435e4d1fcc8b37cf579e4eb494
Tony Lee <tungsheng@gmail.com>
2018-07-02 10:21:57 +0800

    install yarn, neovim,  comment out docker install

--- install/debian/init.sh
+++ install/debian/init.sh
@@ -43,6 +43,15 @@ sudo apt-get -y install silversearcher-ag
 sudo apt-get -y install python-pip
 sudo apt-get -y install apt-transport-https ca-certificates software-properties-common
 
+# Install yarn
+curl -o- -L https://yarnpkg.com/install.sh | bash
+
+# neovim
+echo -ne "Installing neovim...\n"
+sudo apt-get install neovim
+# sudo apt-get install python-neovim
+# sudo apt-get install python3-neovim
+
 # download z.sh (https://github.com/rupa/z)
 echo "Install z...\n"
 [ -f "${HOME}/z.sh" ] && rm -rf ${HOME}/z.sh
@@ -55,6 +64,6 @@ tic $DOTFILES/color/tmux-256color-italic.terminfo
 
 source adduser.sh
 source golang.sh
-source docker.sh
+# source docker.sh
 # source caddy.sh
 
