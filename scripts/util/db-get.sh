1954ae2468f2792a048672923b1abc8da2f21fed
Tony Lee <tungsheng@gmail.com>
2018-07-05 16:32:24 +0800

    update get db data script

--- scripts/util/db-get.sh
+++ scripts/util/db-get.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-source $HOME/.dotfiles/.env
 rm -rf ./tmp
-ssh deploy@$PROD "rm -rf tmp"
+ssh $serv "rm -rf tmp"
 
 # Dump data
-ssh deploy@$PROD "mkdir tmp"
-ssh deploy@$PROD "mongodump -h 127.0.0.1 -d fullinn -o tmp"
+ssh $serv "mkdir tmp"
+ssh $serv "mongodump -h 127.0.0.1 -d fullinn -o tmp"
 
 # Transfer data to local
-scp -r deploy@$PROD:./tmp .
+scp -r $serv:./tmp .
+
+mv tmp/fullinn tmp/$serv
 
 # Restore database
 mongorestore ./tmp --drop
 
 # Clean local and server again
 rm -rf ./tmp
-ssh deploy@$PROD "rm -rf tmp"
+ssh $serv "rm -rf tmp"
 
 exit 0
