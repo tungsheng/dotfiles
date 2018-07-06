9e789f02ff2f70d4fbe423f87ebcc80053ac699a
Tony Lee <tungsheng@gmail.com>
2018-07-07 01:42:03 +0800

    update init script

--- install/debian/init.sh
+++ install/debian/init.sh
@@ -4,7 +4,7 @@ log_file=$HOME/install_progress_log.txt
 echo -ne "Initiating...\n"
 sudo apt-get update
 sudo apt-get -y update
-sudo apt-get -y install openssh-server sudo
+sudo apt-get -y install openssh-server
 
 echo -ne "Installing tmux...\n"
 sudo apt-get install tmux
@@ -49,12 +49,6 @@ sudo apt-get install neovim
 # sudo apt-get install python-neovim
 # sudo apt-get install python3-neovim
 
-# download z.sh (https://github.com/rupa/z)
-echo "Install z...\n"
-[ -f "${HOME}/z.sh" ] && rm -rf ${HOME}/z.sh
-curl -L "https://raw.githubusercontent.com/rupa/z/master/z.sh" -o ~/z.sh
-chmod +x ~/z.sh
-
 echo "Install color...\n"
 tic $DOTFILES/color/xterm-256color-italic.terminfo
 tic $DOTFILES/color/tmux-256color-italic.terminfo
@@ -62,6 +56,6 @@ tic $DOTFILES/color/tmux-256color-italic.terminfo
 source $DOTFILES/install/debian/nvm.sh
 source $DOTFILES/install/debian/golang.sh
 # source $DOTFILES/install/debian/adduser.sh
-# source docker.sh
+source docker.sh
 # source caddy.sh
 
