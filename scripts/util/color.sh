e24c97b9348f2fea9a085d40b4d288fac6eb8f77
Tony Lee <tungsheng@gmail.com>
2018-07-10 01:12:00 +0800

    add color script

---
+++ scripts/util/color.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script echoes colors and codes for bash
+     
+echo -e "n33[4;31mLight Colors33[0m  tt33[1;4;31mDark Colors33[0m"
+     
+echo -e "e[0;30;47m Black    e[0m 0;30m te[1;30;40m Dark Gray  e[0m 1;30m"
+echo -e "e[0;31;47m Red      e[0m 0;31m te[1;31;40m Dark Red   e[0m 1;31m"
+echo -e "e[0;32;47m Green    e[0m 0;32m te[1;32;40m Dark Green e[0m 1;32m"
+echo -e "e[0;33;47m Brown    e[0m 0;33m te[1;33;40m Yellow     e[0m 1;33m"
+echo -e "e[0;34;47m Blue     e[0m 0;34m te[1;34;40m Dark Blue  e[0m 1;34m"
+echo -e "e[0;35;47m Magenta  e[0m 0;35m te[1;35;40m DarkMagentae[0m 1;35m"
+echo -e "e[0;36;47m Cyan     e[0m 0;36m te[1;36;40m Dark Cyan  e[0m 1;36m"
+echo -e "e[0;37;47m LightGraye[0m 0;37m te[1;37;40m White      e[0m 1;37m"
