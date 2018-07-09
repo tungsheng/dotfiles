1fa32090ad702c37c185d653fc069718fbfd9af7
Tony Lee <tungsheng@gmail.com>
2018-07-10 01:11:50 +0800

    update docker script

--- install/debian/docker.sh
+++ install/debian/docker.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
-# switch user
-su docker
 
-# remove older docker
-sudo apt-get -y remove docker docker-engine docker.io
+println() {
+    echo "\n"
+    echo $1
+    echo "...\n"
+}
 
-# setup docker repository
-curl -fsSL https://download.docker.com/linux/debian/gpg  | sudo apt-key add -
-# wget https://download.docker.com/linux/debian/gpg 
-# sudo apt-key add gpg
-echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-cache policy docker-ce
+# remove older docker compose if exist
+which docker-compose || sudo rm /usr/local/bin/docker-compose
 
-# install docker
-sudo apt-get -y install docker-ce
-
-# start docker
-sudo systemctl start docker
-
-# verify docker
-sudo docker run hello-world
-
-
-# remove older docker compose
-sudo rm /usr/local/bin/docker-compose
+# install docker daemon
+output "Install docker daemon"
+wget -qO- https://get.docker.com/ | sh
 
 # install docker compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# show docker compose version
+println "Show docker-compose version"
+docker-compose --version
+
+# create deploy user
+println "create deploy user"
+useradd -m -s /bin/bash deploy
+println "add deploy user to docker group"
+usermod -aG docker deploy
+
+println "docker successfully installed!!"
 
