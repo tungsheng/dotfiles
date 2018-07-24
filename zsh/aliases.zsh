c64934c644fbe44b3402d8a2d72fc0e0bffbcc8d
Tony Lee <tungsheng@gmail.com>
2018-07-24 18:29:03 +0800

    clean up zsh setup, TODO: fix git-completion

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -41,10 +41,3 @@ alias fs="stat -f \"%z bytes\""
 for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
     alias "$method"="lwp-request -m '$method'"
 done
-
-# Kill all the tabs in Chrome to free up memory
-# [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
-alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
-
-alias chrome="/Applications/Google\\ \\Chrome.app/Contents/MacOS/Google\\ \\Chrome"
-alias canary="/Applications/Google\\ Chrome\\ Canary.app/Contents/MacOS/Google\\ Chrome\\ Canary"
