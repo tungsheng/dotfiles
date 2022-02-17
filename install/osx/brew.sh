38f7a3e14a7533e3219a3e264678ad96e2776e28
tungsheng <tungsheng@gmail.com>
2022-02-16 22:56:05 -0800

    update install script

--- install/osx/brew.sh
+++ install/osx/brew.sh
@@ -9,15 +9,9 @@ echo -e "\n\nInstalling homebrew packages..."
 echo "=============================="
 
 # cli tools
-brew install ack
-brew install the_silver_searcher
 brew install tree
 brew install wget
 brew install watchman
-
-# development server setup
-# brew install nginx
-# brew install dnsmasq
 
 # development tools
 brew install git
@@ -26,12 +20,11 @@ brew install hub
 brew install fzf
 brew install reattach-to-user-namespace
 brew install tmux
-brew install ctags
 brew install n
 brew install z
 brew install zsh
 brew install highlight
-brew install ansible
+# brew install ansible
 # brew install vagrant
 
 brew install --cask macdown
@@ -41,7 +34,7 @@ hash zplug 2>/dev/null || git clone https://github.com/zplug/zplug $HOME/.zplug
 
 
 echo -ne "Installing node...\n"
-sudo n 14.0.0
+sudo n latest
 
 echo -ne "Installing neovim...\n"
 brew install neovim
